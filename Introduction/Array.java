55d1e008fe2fdbf05f03376b00fed48ff6ad7b40
RamanKumar656 <ramankp656@gmail.com>
2025-08-04 11:20:13 +0530

    august 4 2025

---
+++ Introduction/Array.java
@@ -0,0 +1,10 @@
+package Introduction;
+
+public class Array {
+    public static void main(String[] args){
+        int[] arr={1,23,3};
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
